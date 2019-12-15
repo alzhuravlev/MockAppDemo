2ef9250d41c2a91ff34fc1b63ac15ae87d325c22
alzhuravlev <crane2002@gmail.com>
2019-12-15 19:46:03 +0300

    ver 2.0.0

--- app/src/main/java/com/crane/mockappdemo/Application.kt
+++ app/src/main/java/com/crane/mockappdemo/Application.kt
@@ -13,19 +13,31 @@ import android.graphics.Bitmap
 import android.graphics.Canvas
 import androidx.core.content.ContextCompat.getSystemService
 import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import com.crane.mockapp.core.model.layouts.AssetsProjectSource
+import com.crane.mockapp.core.model.layouts.LocalProjectSource
+import com.crane.mockapp.core.model.layouts.ProjectSource
 
 
 class Application : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        // Optionally you can override location of the local project's storage.
-        // Default is /sdcard/Documents/MockApp
-        val path =
+        // this allows to inflate layouts from assets folder of your application
+        // just copy folder of your project under /assets/MockApp/
+        val assetsProjectSource = AssetsProjectSource(this)
+
+        val localProjectSource = LocalProjectSource(
+            this,
+            // Optionally you can override location of the local project's storage.
+            // Default is /sdcard/Documents/MockApp
             File(
                 Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS),
-                "MockApp"
-            ).path
-        MockApp.init(this, path)
+                "MockAppDemo"
+            )
+        )
+
+        // Initialize several sources. Order is matter. Projects will be
+        // searched among all sources in order you pass it into init method
+        ProjectSource.init(localProjectSource, assetsProjectSource)
     }
 }
